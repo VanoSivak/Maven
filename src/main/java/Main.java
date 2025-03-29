4c50bfa4e065736345ad701078af9f650d00009d
Ivan Sivakov <Vanya-sivakov@mail.ru>
2025-03-29 14:36:25 +0300

    jUnit/jupiter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,13 @@ public class Main {
 
     public static  void main(String[]args) {
         BonusService service = new BonusService();
-        int bonus = service.calcBonus(5_000, true);
-        System.out.println(bonus);
+
+        int expected = 150;
+        int actual = service.calcBonus(5_000, true);
+        System.out.println("1. " + expected +" == ? == " + actual);
+
+        expected = 50;
+        actual = service.calcBonus(5_000, false);
+        System.out.println("2. " + expected +" == ? == " + actual);
     }
 }
---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BonusServiceTest {
+
+    @Test
+    public void testRegisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        int expected = 150;
+        int actual = service.calcBonus(5_000, true);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testUnregisteredUnderLimit() {
+        BonusService service = new BonusService();
+
+        int expected = 50;
+        int actual = service.calcBonus(5_000, false);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
