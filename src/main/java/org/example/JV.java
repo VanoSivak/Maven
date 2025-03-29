469d730ed9aedb5b19959c3148897d588af72560
Ivan Sivakov <Vanya-sivakov@mail.ru>
2025-03-29 13:33:39 +0300

    Maven

---
+++ src/main/java/org/example/JV.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class JV
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
