8a1e0ed663e89fe201bf0490ffc50b3fa45f602b
Ivan Sivakov <Vanya-sivakov@mail.ru>
2025-04-01 20:22:43 +0300

    Paramet-nie/testi

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,43 +1,26 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class BonusServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "150,5000,true",  // Registered user
-            "0,5000,false"    // Unregistered user should get 0 bonus
-    })
-    public void testBonusCalculation(int expected, int amount, boolean isRegistered) {
-        BonusService service = new BonusService();
-        int actual = service.calcBonus(amount, isRegistered);
-        Assertions.assertEquals(expected, actual);
-    }
-
-}
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvSource;
-//
-//public class BonusServiceTest {
-//
-//    @ParameterizedTest
 //    @CsvSource({
 //            "150,5000,true",
 //            "50,5000,false"
 //    })
-//    public void testRegisteredUnderLimit(int expected, int amount, boolean isRegistered) {
-//        BonusService service = new BonusService();
-//
-//        //int expected = 150;
-//        int actual = service.calcBonus(expected, isRegistered);
-//
-//        Assertions.assertEquals(expected, actual);
-//    }
-//}
+    @CsvFileSource(files="src/test/resources/bonus.csv")
+    public void testRegisteredUnderLimit(int expected, int amount, boolean isRegistered) {
+        BonusService service = new BonusService();
+
+        //int expected = 150;
+        int actual = service.calcBonus(amount, isRegistered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
 //    @Test
 //    public void testUnregisteredUnderLimit() {
 //        BonusService service = new BonusService();
